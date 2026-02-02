477b1f61dffb30a1b17c8ed5e10e8c6fc9ccacd6
Robson Alex <robsonalexmmfata@gmail.com>
2026-02-02 19:19:12 -0300

    tenta

--- src/components/About.tsx
+++ src/components/About.tsx
@@ -5,7 +5,7 @@ import { Download } from "lucide-react";
 
 // Adicione esta linha para importar sua foto de perfil
 // AJUSTE O CAMINHO se 1741032172529.jpg NÃO estiver em src/imgportfolio/
-import minhaFotoDePerfil from '../joso - Copia.jpg';
+import minhaFotoDePerfil from '../img/hero-bg.jpg';
 
 const About = () => {
   return (
